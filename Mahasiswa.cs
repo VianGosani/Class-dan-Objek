7cb630388a2d19c47cb9c7032b197e4171b1963b
VianGosani <48441043+VianGosani@users.noreply.github.com>
2019-03-20 22:50:58 +0700

    Add files via upload
    Contoh Sederhana Class dan Objek

---
+++ Mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanclassdanobjek
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM  : {0}", Nim);
+            Console.WriteLine("IPK  : {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanclassdanobjek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs = new Mahasiswa(); //membuat objek class dari Mahasiswa
+            Mahasiswa mhs2 = new Mahasiswa();
+            mhs.Nama = "Vian"; //mengeset nilai properties objek mhs
+            mhs.Nim = "1811";
+            mhs.Ipk = 3.92f;
+
+            mhs2.Nama = "Gosani"; //mengeset nilai properties objek mhs2
+            mhs2.Nim = "2324";
+            mhs2.Ipk = 3.5f;
+
+            mhs.Registrasi(); //memanggil method registrasi
+            mhs.IsiKrs(); //memanggil method isikrs
+
+            mhs2.Registrasi();
+            mhs2.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
